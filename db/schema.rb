0c50177861bb81cf54f87ebb31925f715436c013
Andy Mangold <andy@friendsoftheweb.com>
2014-03-24 20:10:54 -0400

    Display items on list pages.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322003113) do
+ActiveRecord::Schema.define(version: 20140324233316) do
+
+  create_table "items", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items_lists", force: true do |t|
+    t.integer "list_id"
+    t.integer "item_id"
+  end
 
   create_table "lists", force: true do |t|
     t.string   "title"
